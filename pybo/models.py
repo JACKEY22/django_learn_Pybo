940d87ece3c3f76e041ed223b70fd41d8b93001c
jakey22 <sunjeon7@gmail.com>
2021-01-13 17:05:39 +0900

    plus author

--- pybo/models.py
+++ pybo/models.py
@@ -1,13 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Question(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateField()
-
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateField()
-
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
