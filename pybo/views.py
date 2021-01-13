09fc68711a14653ec60a44d20eb6e8bdc061baa3
jakey22 <sunjeon7@gmail.com>
2021-01-13 17:05:30 +0900

    set page range & login required

--- pybo/views.py
+++ pybo/views.py
@@ -3,11 +3,23 @@ from django.http import HttpResponse
 from pybo.models import Question, Answer
 from pybo.forms import QuestionForm, AnswerForm
 from django.utils import timezone
-
+from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 # Create your views here.
+
+def set_page_range(page_num):
+    start_page = int(((int(page_num) - 1)//10)*10 + 1)
+    end_page = int(((int(page_num) - 1)//10)*10 + 10)
+    return start_page, end_page
+
 def index(request):
+    page_num = request.GET.get('page', '1') # check if param exists, and return 1 if not found /pybo
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list':question_list}
+    paginator = Paginator(question_list, 10)
+    page = paginator.get_page(page_num) 
+
+    start_page, end_page = set_page_range(page_num)
+    context = {'question_list':page, 'start_page':start_page, 'end_page':end_page}
     return render(request, 'pybo/question_list.html', context)
 
 
@@ -18,6 +30,7 @@ def detail(request, question_id):
     context = {'question':question}
     return render(request, 'pybo/question_detail.html', context)
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     # question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now()) # name = 'content'
@@ -29,18 +42,21 @@ def answer_create(request, question_id):
             answer = form.save(commit=False)
             answer.create_date = timezone.now()
             answer.question = question
+            answer.author = request.user
             answer.save()
             return redirect('pybo:detail', question_id=question.id)
     else:
         form = AnswerForm()
     return render(request, 'pybo/question_detail.html', {'question':question, 'form':form})
 
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == 'POST':
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
             question.create_date = timezone.now()
+            question.author = request.user
             question.save()
             return redirect('pybo:index')
     else:
